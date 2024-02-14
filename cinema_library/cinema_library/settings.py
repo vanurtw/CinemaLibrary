916a87cb1e64844927d13c7e1e96ef30a6e54dbd
vano <vanuartw@mail.ru>
2024-02-14 12:23:52 +0300

    movie

--- cinema_library/cinema_library/settings.py
+++ cinema_library/cinema_library/settings.py
@@ -72,11 +72,14 @@ WSGI_APPLICATION = 'cinema_library.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'cinema',
+        'USER': 'cinema_admin',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 

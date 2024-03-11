0c6dc977cfe3dcc9eea3aea0f14eda0c24aa2fa7
vano <vanuartw@mail.ru>
2024-03-11 14:44:40 +0300

    Сделал афмфм

--- cinema_library/api/urls.py
+++ cinema_library/api/urls.py
@@ -8,6 +8,8 @@ from rest_framework import routers
 urlpatterns = [
     path('movie-all/', views.ListViewAPI.as_view()),
     path('categories-all/', views.CategoryAPI.as_view()),
+    path('year-all/', views.YearAPI.as_view()),
+
 
 
 ]
--- cinema_library/api/views.py
+++ cinema_library/api/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from rest_framework.views import APIView
-from .serializers import HomeListSerializer, CategorySerializer
+from .serializers import HomeListSerializer, CategorySerializer, YearSerializer
 from movie.models import Movie, Categories
 from rest_framework.response import Response
+from rest_framework import generics
+from rest_framework import mixins
 
 
 class ListViewAPI(APIView):
@@ -21,3 +23,11 @@ class CategoryAPI(APIView):
 
 class YearAPI(APIView):
     def get(self, request):
+        queryset = Movie.objects.all().distinct()
+        serializer = YearSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+
+
+
+

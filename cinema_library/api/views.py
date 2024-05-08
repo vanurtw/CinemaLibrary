2ab7f71f50bf3f8541310e3fdc51f01d0cb24708
vano <vanuartw@mail.ru>
2024-05-08 12:04:01 +0300

    pagination

--- cinema_library/api/views.py
+++ cinema_library/api/views.py
@@ -6,12 +6,9 @@ from rest_framework.response import Response
 from rest_framework import generics
 from rest_framework import mixins
 from rest_framework import viewsets
+from rest_framework.pagination import PageNumberPagination
 
 
 class MovieViewsets(viewsets.ReadOnlyModelViewSet):
     serializer_class = MovieSerializer
     queryset = Movie.objects.all()
-
-    def list(self, request, *args, **kwargs):
-        serializer = self.serializer_class(self.queryset, many=True)
-        return Response(serializer.data)
--- cinema_library/cinema_library/settings.py
+++ cinema_library/cinema_library/settings.py
@@ -87,6 +87,12 @@ DATABASES = {
     }
 }
 
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 2
+}
+
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 

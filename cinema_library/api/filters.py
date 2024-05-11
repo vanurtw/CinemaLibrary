9825233cf87e1955ecf83eafbbe7679e87605031
vano <vanuartw@mail.ru>
2024-05-11 09:42:13 +0300

    filter ok

--- cinema_library/api/filters.py
+++ cinema_library/api/filters.py
@@ -5,7 +5,8 @@ from movie.models import Movie, Genre
 class MovieFilter(django_filters.FilterSet):
     genre = django_filters.CharFilter(field_name='genre', lookup_expr='slug__icontains')
     title = django_filters.CharFilter(field_name='title', lookup_expr='icontains')
+    category = django_filters.CharFilter(field_name='category', lookup_expr='slug__icontains')
 
     class Meta:
         model = Movie
-        fields = ['rating', 'genre', 'title']
+        fields = ['rating', 'genre', 'title', 'category', 'premiere']
--- cinema_library/api/serializers.py
+++ cinema_library/api/serializers.py
@@ -5,6 +5,6 @@ from movie.models import Categories, Movie
 class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
-        fields = ['id', 'title', 'tagline', 'rating', 'poster', 'genre']
+        fields = ['id', 'title', 'tagline', 'rating', 'poster', 'genre', 'slug']
         depth = 2
 
